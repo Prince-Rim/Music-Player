122c2cdbc87c644984332428f9d5db14b5679d00
Justin Allen Azucena <azucenajustinallen@gmail.com>
2025-07-08 23:36:16 +0800

    Add files via upload
    Educational Purposes

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "This Side of Paradise",
+    artist: "Coyote Theory",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Rather Be",
+    artist: "Clean Bandits",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Heat Waves",
+    artist: "Glass Animals",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Pompeii",
+    artist: "Bastille",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Notion",
+    artist: "The Rare Occasions",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "High Hopes",
+    artist: "Panic!At The Disco",
+    img: "music-6",
+    src: "music-6"
+  },
+];
